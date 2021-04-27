c8cf76d84f5f8fb9bb1fae0f950bc599b841bfb2
Kant <>
2021-04-27 22:45:34 +0900

    Compression Resistance

---
+++ CompressionResistance/CompressionResistance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompressionResistance
+//
+//  Created by Kant on 2021/04/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
