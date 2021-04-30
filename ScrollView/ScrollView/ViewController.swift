12ac4fb64d3cb1d1e97b0732d24dccdfbf8610aa
sapere4ude <eileenyoo2@gmail.com>
2021-04-30 22:01:07 +0900

    ScrollView

---
+++ ScrollView/ScrollView/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ScrollView
+//
+//  Created by Kant on 2021/04/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var img: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        img.image = UIImage(named: "aaaaa")
+    }
+
+
+}
+
