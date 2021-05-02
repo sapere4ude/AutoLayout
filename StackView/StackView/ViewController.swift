37311eb248081f9aa618fe1060ab8da2a68de61f
sapere4ude <eileenyoo2@gmail.com>
2021-05-03 00:35:53 +0900

    StackView / 전화번호 입력

--- StackView/StackView/ViewController.swift
+++ StackView/StackView/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    
+    @IBOutlet var phoneNumberBtns: [CircleButton]!
+    
+    @IBOutlet weak var phoneCallBtn: CircleButton!
+    
+    var phoneNumberString = "" {
+        didSet {
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                self.phoneNumberLabel.textColor = .black
+                self.phoneNumberLabel.text = self.phoneNumberString
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for btnItem in phoneNumberBtns {
+            btnItem.addTarget(self, action: #selector(onNumberBtnClicked(sender:)), for: .touchUpInside)
+        }
+        
+        phoneCallBtn.addTarget(self, action: #selector(onPhoneCallBtnClicked(_:)), for: .touchUpInside)
     }
-
+    
+    @objc fileprivate func onNumberBtnClicked(sender: UIButton) {
+        guard let inputString = sender.titleLabel?.text else { return }
+        phoneNumberString.append(inputString)
+    }
+    
+    @objc fileprivate func onPhoneCallBtnClicked(_ sender: UIButton) {
+        phoneNumberString.removeAll()
+        
+    }
 
 }
 
